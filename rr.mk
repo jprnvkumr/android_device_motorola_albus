34050128fea35c540987e843d4b7e3ce7a64d66e
Pranav Kumar <jprnvkumr@gmail.com>
2019-08-29 19:40:24 +0530

    boot_animation resolution and build_fingerprint

--- rr.mk
+++ rr.mk
@@ -22,8 +22,7 @@ $(call inherit-product, device/motorola/albus/full_albus.mk)
 $(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 # Boot animation
-TARGET_SCREEN_WIDTH := 1080
-TARGET_SCREEN_HEIGHT := 1920
+TARGET_BOOT_ANIMATION_RES := 720
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := albus
@@ -37,4 +36,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=albus \
     PRIVATE_BUILD_DESC="albus-user 8.0.0 OPSS27.76-12-25-3 4 release-keys"
 
-BUILD_FINGERPRINT := google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys
+BUILD_FINGERPRINT := google/ryu/dragon:8.1.0/OPM8.190605.005/5749003:user/release-keys
